c823dce65b2d2aa882c351a6262d0222bf485e43
Asif Ali <shea786@live.co.uk>
2017-08-26 01:07:15 +0000

    added login pages with router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,83 @@
 <template>
-  <v-app>
-    <navbar></navbar>
+  <v-app id="example-2" toolbar>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      light
+      overflow
+      absolute
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon dark @click.native.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed class="deep-orange" dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Warwickshire CCVI</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat to="/login">
+          <v-icon left>unlock_open</v-icon>
+          Sign In
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn flat to="/register">
+          <v-icon left>face</v-icon>
+          Sign Up
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
   </v-app>
 </template>
 
 <script>
-  import Navbar from './components/Navbar.vue'
+  import Login from './components/authentication/Login.vue'
+  import Register from './components/authentication/Register.vue'
   export default {
     components: {
-      'navbar': Navbar
+      'login': Login,
+      'register': Register
+    },
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        mini: false,
+        right: null
+      }
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import Router from './routes.js'
 
 Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: Router
 })
