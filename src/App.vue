9fd5b75210fbf5b8c0b5e6efbb66e8d5adcf6cc7
Asif Ali <shea786@live.co.uk>
2017-08-26 00:09:39 +0000

    added vue.js as base app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <navbar></navbar>
+  </v-app>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default {
+    components: {
+      'navbar': Navbar
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
